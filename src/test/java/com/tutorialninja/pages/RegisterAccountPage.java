5378df0b98f5678130f57ed91f903ef9631f2526
MahbubulRishad <mahbubulrishad@gmail.com>
2025-01-27 00:22:23 +0600

    DDT implementation

--- src/test/java/com/tutorialninja/pages/RegisterAccountPage.java
+++ src/test/java/com/tutorialninja/pages/RegisterAccountPage.java
@@ -28,31 +28,50 @@ public class RegisterAccountPage extends BasePage {
         return this;
     }
 
-    public RegisterAccountPage fillTelephoneNo(String telephoneNo){
+    public RegisterAccountPage fillTelephoneNo(String telephoneNo) {
         getWebElement(By.xpath("//label[text() = 'Telephone']/following-sibling::div/input[@id = 'input-telephone']")).sendKeys(telephoneNo);
         return this;
     }
 
-    public RegisterAccountPage fillPassword(String password){
+    public RegisterAccountPage fillPassword(String password) {
         getWebElement(By.xpath("//label[text() = 'Password']/following-sibling::div/input[@id = 'input-password']")).sendKeys(password);
         return this;
     }
 
-    public RegisterAccountPage fillConfirmPassword(String confirmPassword){
+    public RegisterAccountPage fillConfirmPassword(String confirmPassword) {
         getWebElement(By.xpath("//label[text() = 'Password Confirm']/following-sibling::div/input[@id = 'input-confirm']")).sendKeys(confirmPassword);
         return this;
     }
 
-    public RegisterAccountPage clickOnSubscribeYesRadioBtn(){
+    public RegisterAccountPage clickOnSubscribeYesRadioBtn() {
         getWebElement(By.xpath("(//label[contains(text(), 'Subscribe')]/following-sibling::div/label/input)[1]")).click();
         return this;
     }
 
-    public RegisterAccountPage clickOnPrivacyPolicyCheckBox(){
+
+    public RegisterAccountPage clickOnSubscribeNoRadioBtn() {
+        getWebElement(By.xpath("(//label[contains(text(), 'Subscribe')]/following-sibling::div/label/input)[2]")).click();
+        return this;
+    }
+
+    public RegisterAccountPage selectNewsletterSubscribe(String subscription) {
+
+        if (subscription.equalsIgnoreCase("yes")){
+            clickOnSubscribeYesRadioBtn();
+        } else {
+            clickOnSubscribeNoRadioBtn();
+        }
+
+            return this;
+    }
+
+
+    public RegisterAccountPage clickOnPrivacyPolicyCheckBox() {
         getWebElement(By.xpath("//a[@class = 'agree']/following-sibling::input[@type='checkbox']")).click();
         return this;
     }
-    public AccountSuccessPage clickOnContinueBtn(){
+
+    public AccountSuccessPage clickOnContinueBtn() {
         getWebElement(By.xpath("//a[@class = 'agree']/following-sibling::input[@value='Continue']")).click();
         return getInstance(AccountSuccessPage.class);
     }
--- src/test/java/com/tutorialninja/test/testCases/RegisterAccountPage.java
+++ src/test/java/com/tutorialninja/test/testCases/RegisterAccountPage.java
@@ -3,27 +3,34 @@ package com.tutorialninja.test.testCases;
 import com.tutorialninja.pages.AccountSuccessPage;
 import com.tutorialninja.pages.IndexPage;
 import com.tutorialninja.test.base.BaseTest;
+import com.tutorialninja.util.General;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class RegisterAccountPage extends BaseTest {
-    @Test
-    public void verifyRegistrationShouldBeSuccessful(){
+    @Test (dataProvider = "getData")
+    public void verifyRegistrationShouldBeSuccessful(String fname, String lname, String email, String tel, String pw, String cpw, String subLetter){
 
         AccountSuccessPage accountSuccessPage = page.getInstance(IndexPage.class)
                 .clickOnMyAccountMenu()
                 .clickOnRegisterLink()
-                .fillFirstName(lorem.getFirstName())
-                .fillLastName(lorem.getLastName())
-                .fillEmail(lorem.getEmail())
-                .fillTelephoneNo(String.valueOf(lorem.getPhone()))
-                .fillPassword("12345")
-                .fillConfirmPassword("12345")
-                .clickOnSubscribeYesRadioBtn()
+                .fillFirstName(fname)
+                .fillLastName(lname)
+                .fillEmail(email)
+                .fillTelephoneNo(tel.replaceAll(".0", "").trim())
+                .fillPassword(pw)
+                .fillConfirmPassword(cpw)
+                .selectNewsletterSubscribe(subLetter)
                 .clickOnPrivacyPolicyCheckBox()
                 .clickOnContinueBtn();
 
         Assert.assertTrue(accountSuccessPage.hasSuccessMessageDisplayed());
+    }
+
+    @DataProvider
+    public Object[][] getData(){
+        return General.getTestData("Sheet1");
     }
 
 }
